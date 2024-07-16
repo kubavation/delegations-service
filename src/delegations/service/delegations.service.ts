37e0abe8fc54ed67a4d8557885ea72fd5d33ca7f
kubavation <37585627+kubavation@users.noreply.github.com>
2024-07-16 19:58:01 +0200

    crud delegation service

--- src/delegations/service/delegations.service.ts
+++ src/delegations/service/delegations.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from "@nestjs/common";
+import {Injectable, NotFoundException} from "@nestjs/common";
 import {InjectModel} from "@nestjs/sequelize";
 import {Delegation} from "../model/delegation";
 
@@ -8,4 +8,39 @@ export class DelegationsService {
     constructor(@InjectModel(Delegation) private readonly delegation: typeof Delegation) {
     }
 
+
+    async findAll() {
+        return this.delegation.findAll();
+    }
+
+    async findOne(id: string) {
+
+        const delegation = await this.delegation.findOne({
+            where: { id }
+        })
+
+        if (delegation) {
+            throw new NotFoundException(`Delegation with id ${id} not found`);
+        }
+
+        return delegation;
+    }
+
+    async create(data: Delegation) { //todo DTO
+        return this.delegation.create(data);
+    }
+
+    async update(id: string, data: Delegation) {
+
+        const delegation = await this.findOne(id);
+
+        return delegation.update(data);
+    }
+
+    async remove(id: string) {
+
+        const delegation = await this.findOne(id);
+
+        await delegation.destroy();
+    }
 }
