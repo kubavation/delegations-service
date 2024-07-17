365292381b2369e88da142f0c2f632ac02cfa728
kubavation <37585627+kubavation@users.noreply.github.com>
2024-07-17 20:02:30 +0200

    sequelize configuration

--- src/delegations/delegations.module.ts
+++ src/delegations/delegations.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DelegationsController } from './controller/delegations.controller';
+import {DelegationsService} from "./service/delegations.service";
+import {SequelizeModule} from "@nestjs/sequelize";
+import {Delegation} from "./model/delegation";
+
 
 @Module({
-  controllers: [DelegationsController]
+  imports: [SequelizeModule.forFeature([Delegation])],
+  controllers: [DelegationsController],
+  providers: [DelegationsService]
 })
 export class DelegationsModule {}
